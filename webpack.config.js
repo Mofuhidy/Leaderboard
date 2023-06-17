285f6bf2c222dc69b3c7202ee81aba4ed3647b87
moFuhidy <mo7ammed9290@gmail.com>
2023-06-17 13:15:49 +0300

    Update the configration

--- webpack.config.js
+++ webpack.config.js
@@ -3,6 +3,9 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
   mode: 'development',
+  devServer: {
+    static: './dist',
+  },
   entry: {
     index: './src/index.js',
   },
