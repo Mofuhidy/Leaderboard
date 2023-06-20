018083e89e4ddbbe8d0930e42f1f2ff35d22b5cb
moFuhidy <mo7ammed9290@gmail.com>
2023-06-20 13:09:16 +0300

    Add sweet alert for confiramtion of add functionality

--- src/modules/apiOperations.js
+++ src/modules/apiOperations.js
@@ -1,16 +1,41 @@
+// eslint-disable-next-line import/no-extraneous-dependencies
+import Swal from 'sweetalert2';
+
 const gameId = '8kPKQdZ1a1fTYN9LxNsH';
 
 const apiUrl = `https://us-central1-js-capstone-backend.cloudfunctions.net/api/games/${gameId}/scores/`;
 
 const createScore = async (user, score) => {
-  const response = await fetch(apiUrl, {
-    method: 'POST',
-    headers: {
-      'Content-Type': 'application/json',
-    },
-    body: JSON.stringify({ user, score }),
-  });
-  return response;
+  try {
+    const response = await fetch(apiUrl, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({ user, score }),
+    });
+    const res = await response.json();
+    if (response.ok) {
+      Swal.fire({
+        position: 'top-end',
+        icon: 'success',
+        title: res.result,
+        showConfirmButton: false,
+        timer: 1500,
+      });
+      return res.result;
+    }
+    return null; // Return a default value when the response is not successful
+  } catch (error) {
+    Swal.fire({
+      position: 'top-end',
+      icon: 'error',
+      title: error,
+      showConfirmButton: false,
+      timer: 1500,
+    });
+    return null;
+  }
 };
 
 const getScore = async () => {
